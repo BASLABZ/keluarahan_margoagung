3356d7b38accc8e241639c768698e7adb7552711
SuperBase <tutorialsuper.cs@gmail.com>
2017-06-01 21:00:15 +0700

    setting

--- administrator/admin/koneksi/koneksi.php
+++ administrator/admin/koneksi/koneksi.php
@@ -2,7 +2,7 @@
 	$host = 'localhost';
 	$user = 'root';
 	$pass = '';
-	$dbname = 'db_kelurahan';
+	$dbname = 'db_keluarahan_margoaung';
 	$koneksi = mysql_connect($host, $user, $pass) or die(mysql_error());
 	$dbselect = mysql_select_db($dbname) or die(mysql_error());
  ?>
